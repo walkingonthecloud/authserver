58eae458b90ac7f6dd38f0ad33134b58bc089bd9
Srinivasan Varadharajan <minnal_srinivas@yahoo.com>
2019-12-26 20:17:26 +0530

    Local JWT validations :(

--- src/main/java/com/pilogix/authserver/config/WebSecurityConfig.java
+++ src/main/java/com/pilogix/authserver/config/WebSecurityConfig.java
@@ -56,7 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
 
     // dont authenticate this particular request
-        .authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
+        .authorizeRequests().antMatchers("/getToken", "/register").permitAll().
 
     // all other requests need to be authenticated
         anyRequest().authenticated().and().
